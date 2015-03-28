0a0ccfdcc61d332ef46b38493f8ae4d339873e5a
Haffi <hafthort12@ru.is>
2015-03-28 00:43:48 +0000

    prufa meira

--- main.c
+++ main.c
@@ -168,7 +168,7 @@ static void customer_arrived(struct customer *customer, void *arg)
         ELSE
             recjecta viðskiptavin
     */
-    printf("Front: %c", chairs->barberShop.front);
+    printf("Front: %d \n", chairs->barberShop.front);
     //if- setningin
     if(thrlab_get_num_chairs() == 0)
     {
