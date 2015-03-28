8f4789596edda8bf0bd39b14a4f41c87642e1553
arnara13 <arnara13@skel.ru.is>
2015-03-28 17:36:15 +0000

    Comment eydd ut

--- main.c
+++ main.c
@@ -31,31 +31,21 @@ typedef struct
     sem_t items;    // Counts available items for use
 } sbuf_t;
 
-//Halda utan um biðstólana í stofunni
 struct chairs
 {
-    //struct customer **customer; /* Array of customers */
-    int max;
-    //Leid til ad læsa þegar vid erum ad uppfæra 
-    //t.d customer (fyrir ofan) gagnagrindina
+    int max;    
     sem_t mutex;
     sem_t chair; 
     sem_t barber;
-
     sbuf_t barberShop; // Circular buffer - contains customer info if not empty
-
-    /* TODO: Add more variables related to threads */
-    /* Hint: Think of the consumer producer thread problem */
 };
 
-//Halda utan um barber
 struct barber
 {
     int room;
     struct simulator *simulator;
 };
 
-//Halda utan um allt saman a einum stad
 struct simulator
 {
     struct chairs chairs;
@@ -87,10 +77,7 @@ static void setup(struct simulator *simulator)
     //Upphafsstilla mutex, chair og barber
     sem_init(&chairs->mutex, 0, 1);
     sem_init(&chairs->chair, 0, thrlab_get_num_chairs());
-    sem_init(&chairs->barber, 0, 0); 
-
-    /* Create chairs*/
-    // chairs->customer = malloc(sizeof(struct customer *) * thrlab_get_num_chairs());
+    sem_init(&chairs->barber, 0, 0);
 
     // Sbuf circular buffer for waiting room chairs
     sbuf_init(&chairs->barberShop, thrlab_get_num_chairs());
@@ -118,8 +105,6 @@ static void setup(struct simulator *simulator)
 static void cleanup(struct simulator *simulator)
 {
     /* Free chairs */
-    //free(simulator->chairs.customer);
-    // free(simulator->chairs.barberShop);
     sbuf_deinit(&simulator->chairs.barberShop);
 
     /* Free barber thread data */
@@ -135,66 +120,28 @@ static void customer_arrived(struct customer *customer, void *arg)
     struct simulator *simulator = arg;
     struct chairs *chairs = &simulator->chairs;
     
-    //Byr til semaphoru(semaphora er lás - hleypur i gegn einum eda fleiri eftir hvernig vid skilgreinum lasinn)
-    //sem_init er constructor fyrir semaphoru 
     sem_init(&customer->mutex, 0, 0);
-    //Passa ad vid seum ekki ad samþykkja alla vidskiptavini - þarf að visa fra þeim sem komast ekki i sætid
-     
-    
-    /* TODO: Accept if there is an available chair */
-    //Bida eftir lausum stól
-    //IFSETNINGsem_wait(&chairs->chair);
-    //Passa ad þad se bara einn sem ma breyta semaphorunni (gagnagrindinni) í einu
-    //IFSETNINGsem_wait(&chairs->mutex);
-    //IFSETNINGthrlab_accept_customer(customer);
-    //Halda utan um customer - finna bestu leid til ad halda utan um (ekki stack!)
-    //engin virkni eins og er - tarf ad utfaera tad 
-    //IFSETNINGchairs->customer[0] = customer;
 
-    //Ferlid fra TO DO er ad vidskiptavinur kemur og bidur, 
-    //eg samþykki hann og nu leyfi eg öðrum að koma inn:
-    //IFSETNINGsem_post(&chairs->mutex);
-     
-    //IFSETNINGsem_post(&chairs->barber); 
-    //Passa ad customer stingi ekki af - halda honum - getum læst honum 
-    //IFSETNINGsem_wait(&customer->mutex); //nu er hann læstur
-    
-    /* TODO: Reject if there are no available chairs */
-    //if setning sem segir 'ef tad er laust plass , ta accepta eg, annars rejecta tessum vidskiptavin
-    //thrlab_reject_customer(customer);
-    
-    
     /* --- Sauðakóði
         IF (laust pláss)
             accepta viðskiptavin
         ELSE
             recjecta viðskiptavin
     */
-
     int slts;
     int itm;
     sem_getvalue(&chairs->barberShop.slots, &slts);
     sem_getvalue(&chairs->barberShop.items, &itm);
 
-
-    // printf("Front: %d \n", chairs->barberShop.front);
-    // printf("Rear: %d \n", chairs->barberShop.rear);
-    // printf("Slots: %d \n", slts);
-    // printf("Items: %d \n", itm);
-
     //if- setningin
     if(slts != 0)
     {
-        //sem_wait(&customer->mutex);
     	sem_wait(&chairs->chair);
     	sem_wait(&chairs->mutex);
 
         sbuf_insert(&chairs->barberShop, customer);
 
         thrlab_accept_customer(customer);
-    	// chairs->customer[0] = customer; // Ath laga Allir viðskipavinir yfirskrifa hvaða viðskiptavinur kemur næst
-
-        // printf("value: %d", *chairs->barberShop.buf);
 
     	sem_post(&chairs->mutex);
     	sem_post(&chairs->barber);
@@ -211,28 +158,23 @@ static void *barber_work(void *arg)
 {
     struct barber *barber = arg;
     struct chairs *chairs = &barber->simulator->chairs;
-    struct customer *customer; /* TODO: Fetch a customer from a chair */
+    struct customer *customer;
 
     /* Main barber loop */
-    //
     while (true) 
     {
         sem_wait(&chairs->barber);
         sem_wait(&chairs->mutex);
-        /* TODO: Here you must add you semaphores and locking logic */
-
-        // customer = chairs->customer[0];     /* TODO: You must choose the customer */
 
         customer = sbuf_remove(&chairs->barberShop);
 
         thrlab_prepare_customer(customer, barber->room);
-        sem_post(&chairs->mutex);           //bætt vid i fyrirlestri
-        sem_post(&chairs->chair);           //bætt vid, er samt i raun vitlaust , þvi vid turfum ad visa fra ef ekki laust sæti
+        sem_post(&chairs->mutex);
+        sem_post(&chairs->chair);
         	
         thrlab_sleep(5 * (customer->hair_length - customer->hair_goal));
         thrlab_dismiss_customer(customer, barber->room);
        
-    	//Hækka semaphoruna / mun hleypa þessum customer af stad. Hleypa customer ur stofunni
     	sem_post(&customer->mutex);
     }
     return NULL;
@@ -241,7 +183,6 @@ static void *barber_work(void *arg)
 void sbuf_init(sbuf_t *sp, int n)
 {
     sp->buf = calloc(n, sizeof(int));
-    //sp->buf = calloc(n, sizeof(struct customer *));
     sp->n = n;                      // Buffer holds max of n items
     sp->front = sp->rear = 0;       // Empty buffer is front == rear
     sem_init(&sp->mutex, 0, 1);     // Binary semaphore for locking
@@ -260,12 +201,12 @@ void sbuf_insert(sbuf_t *sp, struct customer *customer)
 {
     P(&sp->slots);                              // Wait for available slot
     P(&sp->mutex);                              // Lock the buffer
-    sp->buf[(++sp->rear) % (sp->n)] = customer;     // Insert the item
+    sp->buf[(++sp->rear) % (sp->n)] = customer; // Insert the item
     V(&sp->mutex);                              // Unlock the buffer
     V(&sp->items);                              // Announce available item
 }
 
-// Remove and return the first item for buffer sp                   ATH Þarf möguleg að skila struct en ekki int
+// Remove and return the first item for buffer sp
 struct customer* sbuf_remove(sbuf_t *sp)
 {
     struct customer *item;
