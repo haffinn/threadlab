0db9a3d53eb0e8cdccb6f42ed96d71e36911849a
Haffi <hafthort12@ru.is>
2015-03-28 04:16:51 +0000

    test

--- main.c
+++ main.c
@@ -119,7 +119,8 @@ static void cleanup(struct simulator *simulator)
 {
     /* Free chairs */
     //free(simulator->chairs.customer);
-    free(simulator->chairs.barberShop.buf);
+    // free(simulator->chairs.barberShop);
+    sbuf_deinit(simulator->chairs.barberShop.buf);
 
     /* Free barber thread data */
     free(simulator->barber);
